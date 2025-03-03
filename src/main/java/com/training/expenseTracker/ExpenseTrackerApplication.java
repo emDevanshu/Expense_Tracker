ee8b1d1682ba103ececbaed3162d50b097796dcf
Devanshu Sharma <devanshu.sharma@sap.com>
2025-03-03 15:49:11 +0530

    changes done

--- src/main/java/com/training/expenseTracker/ExpenseTrackerApplication.java
+++ src/main/java/com/training/expenseTracker/ExpenseTrackerApplication.java
@@ -4,7 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class ExpenseTrackerApplication extends SpringBootServletInitializer {
 
@@ -12,8 +15,8 @@ public class ExpenseTrackerApplication extends SpringBootServletInitializer {
 		SpringApplication.run(ExpenseTrackerApplication.class, args);
 	}
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(ExpenseTrackerApplication.class);
+	@GetMapping("/hello")
+	public String display(){
+		return "Welcome to Devanshu Sharma's Expense Tracker Application";
 	}
 }
--- src/main/java/com/training/expenseTracker/controller/UserController.java
+++ src/main/java/com/training/expenseTracker/controller/UserController.java
@@ -1,40 +1,40 @@
-package com.training.expenseTracker.controller;
-
-import com.training.expenseTracker.model.User;
-import com.training.expenseTracker.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-
-@RestController
-@RequestMapping("/api/v1/user")
-public class UserController {
-
-    @Autowired
-    UserService userService;
-
-    @GetMapping("/welcome/{name}")
-    public String welcome(@PathVariable String name) {
-        return "Welcome to the project, " + name + "!!!";
-    }
-
-    @GetMapping("/show")
-    public List<User> getAllUsers() {
-        return userService.getAllUsers();
-    }
-
-    @PostMapping("/register")
-    public ResponseEntity<?> registerUser(@RequestBody User user) {
-        userService.registerUser(user);
-        return new ResponseEntity<>("User has been Registered Successfully", HttpStatus.CREATED);
-    }
-
-    @PostMapping("/login")
-    public ResponseEntity<?> loginUser(@RequestParam String email, @RequestParam String password) {
-        userService.loginUser(email, password);
-        return new ResponseEntity<>("User successfull login", HttpStatus.CREATED);
-    }
-}
+//package com.training.expenseTracker.controller;
+//
+//import com.training.expenseTracker.model.User;
+//import com.training.expenseTracker.service.UserService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
+//
+//import java.util.List;
+//
+//@RestController
+//@RequestMapping("/api/v1/user")
+//public class UserController {
+//
+//    @Autowired
+//    UserService userService;
+//
+//    @GetMapping("/welcome/{name}")
+//    public String welcome(@PathVariable String name) {
+//        return "Welcome to the project, " + name + "!!!";
+//    }
+//
+//    @GetMapping("/show")
+//    public List<User> getAllUsers() {
+//        return userService.getAllUsers();
+//    }
+//
+//    @PostMapping("/register")
+//    public ResponseEntity<?> registerUser(@RequestBody User user) {
+//        userService.registerUser(user);
+//        return new ResponseEntity<>("User has been Registered Successfully", HttpStatus.CREATED);
+//    }
+//
+//    @PostMapping("/login")
+//    public ResponseEntity<?> loginUser(@RequestParam String email, @RequestParam String password) {
+//        userService.loginUser(email, password);
+//        return new ResponseEntity<>("User successfull login", HttpStatus.CREATED);
+//    }
+//}

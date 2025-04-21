dfc42e07258be227930f2eef74dee8d3f2cea1f1
Devanshu Sharma <devanshu.sharma@sap.com>
2025-04-21 10:12:08 +0530

    cors origin added for angular in the springboot

--- expense-tracker-service/src/main/java/com/training/expenseTracker/ExpenseTrackerApplication.java
+++ expense-tracker-service/src/main/java/com/training/expenseTracker/ExpenseTrackerApplication.java
@@ -1,12 +1,19 @@
 package com.training.expenseTracker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ExpenseTrackerApplication extends SpringBootServletInitializer {
+
+	@Value("${frontend.origin}")
+	public String frontendOrigin;
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
@@ -15,6 +22,19 @@ public class ExpenseTrackerApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExpenseTrackerApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins(frontendOrigin)
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+						.allowCredentials(true);
+			}
+		};
 	}
 }
 
